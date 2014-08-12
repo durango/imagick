9c683559dbe6ffd370b79d341783a80fe008da38
Daniel Durante <me@danieldurante.com>
2014-08-12 18:59:19 -0400

    Fix for 32 bit machines, just source env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-export CGO_CFLAGS="-I`pkg-config --cflags MagickWand`"
-export CGO_LDFLAGS="-I`pkg-config --libs MagickWand`"
+export CGO_CFLAGS="-I`pkg-config --cflags MagickWand` -D MAGICKCORE_SIZEOF_DOUBLE=12"
+export CGO_LDFLAGS="-I`pkg-config --libs MagickWand` -D MAGICKCORE_SIZEOF_DOUBLE=12"
